6e78543845d2049b445ea21396ec35cd6d5cdcba
Saptarshi-bits <2025sl93026@wilp.bits-pilani.ac.in>
2026-05-08 19:46:37 +0530

    Print statements added

--- calculator.py
+++ calculator.py
@@ -11,3 +11,10 @@ def divide(a, b):
     if b == 0:
         return None
     return a / b
+
+
+
+print("Add:", add(10, 5))
+print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
+print("Divide:", divide(10, 5))
